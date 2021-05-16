535393e679c8e0322fca7af7f6699129d799c6ba
iLyas Farawe <faraweilyas@gmail.com>
2021-05-16 18:17:45 +0100

    Configures keys

--- app/Config/core.php
+++ app/Config/core.php
@@ -160,7 +160,7 @@
  *	`admin_index()` and `/admin/controller/index`
  *	`manager_index()` and `/manager/controller/index`
  */
-	//Configure::write('Routing.prefixes', array('admin'));
+Configure::write('Routing.prefixes', array('admin'));
 
 /**
  * Turn off all caching application-wide.
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'aGn3UCnBPziAdwMDOGQCwYaSLZ846sdoUDQp8Cfk');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '243259111076882134580270227469');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
